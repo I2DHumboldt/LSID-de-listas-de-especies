d3cea44061d63872f485c53c208cc24b9b150310
Juan Rey <jrey@humboldt.org.co>
2022-08-17 11:08:44 -0500

    actualizacion de funcion solo para GBIFID

--- id.R
+++ id.R
@@ -1,25 +1,3 @@
-
-###############
-##Get the NameID codes from Tropicos for taxonomic names.
-# If you wantm you can use this different data base:
-# db = c("itis", "ncbi", "eol", "col", "tropicos", "gbif", "nbn")
-#############
-# Abrir libreria
-  library(taxize)
-#Abro espacio de trabajo
-setwd("C:/Users/juan.rey/Desktop")
-# Abro archivo de trabajo
-id <- read.csv("lsid_test.csv", head=TRUE, sep=";")
-#Ejecutar el siguiente codigo para obtener ID de Tropicos for taxonomic names
-spnames <- as.factor(id$scientificName)
-class(spnames)
-test<-get_tpsid(spnames, rows=1)
-idfinal<-data.frame(spnames, test)
-
-# Exporta tabla de ID
-write.table(idfinal, file = "List_final.csv", sep = ",", col.names = NA,
-            qmethod = "double")
-
 #################################
 # Retrieve taxonomic identifiers for a given taxon name based on "itis"
 # Se puede usar las siguientes bases de datos db = c("itis", "ncbi", "eol", "col", "tropicos", "gbif", "nbn")
@@ -27,17 +5,15 @@ write.table(idfinal, file = "List_final.csv", sep = ",", col.names = NA,
 # Abrir libreria
 library(taxize)
 #Abro espacio de trabajo
-setwd("C:/Users/juan.rey/Desktop")
+setwd("~/miruta")
 # Abro archivo de trabajo
-id <- read.csv("lsid_test.csv", head=TRUE, sep=";")
+id <- read.csv("misdatos.csv", fileEncoding = "UTF-8")
 #Ejecutar el siguiente codigo para obtener ID de Tropicos for taxonomic names
-spnames <- as.factor(id$scientificName)
+spnames <- as.character(id$scientificName)
 class(spnames)
 test1<-get_ids(spnames, db=c("gbif"), rows=1, verbose=TRUE)
 idfinal1<-data.frame(cbind(test1$gbif))
-as<-paste("itis.gov:servlet:SingleRpt:SingleRpt?search_topic=TSN&search_value=", idfinal1$cbind.test1.itis. ,
-          ifelse(is.na(idfinal1$cbind.test1.itis.), "No found", idfinal1$cbind.test1.itis.), sep="")
-a<- data.frame(idfinal1, as)
-# Exporta tabla de ID
-write.table(a, file = "List_final.csv", sep = ",", col.names = NA,
-            qmethod = "double")
+idfin<- data.frame(species=row.names(idfinal1), idfinal1, row.names=NULL, check.names=FALSE)
+idfin$cbind.test1.gbif.
+as<-paste("gbif.org:species:", idfin$cbind.test1.gbif. ,sep="")
+a<- data.frame(idfin, as)
