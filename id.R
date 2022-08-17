73f7f90ba66940ed2ad0c4f563393382fd56977c
Camila Diaz <mdiaz@humboldt.org.co>
2022-08-17 11:53:08 -0500

    borrar lineas

--- id.R
+++ id.R
@@ -1,25 +1,3 @@
-
-###############
-##Get the NameID codes from Tropicos for taxonomic names.
-# If you wantm you can use this different data base:
-# db = c("itis", "ncbi", "eol", "col", "tropicos", "gbif", "nbn")
-#############
-# Abrir libreria
-  library(taxize)
-#Abro espacio de trabajo
-setwd("C:/Users/juan.rey/Desktop")
-# Abro archivo de trabajo
-id <- read.csv("lsid_test.csv", head=TRUE, sep=";")
-#Ejecutar el siguiente codigo para obtener ID de Tropicos for taxonomic names
-spnames <- as.factor(id$scientificName)
-class(spnames)
-test<-get_tpsid(spnames, rows=1)
-idfinal<-data.frame(spnames, test)
-
-# Exporta tabla de ID
-write.table(idfinal, file = "List_final.csv", sep = ",", col.names = NA,
-            qmethod = "double")
-
 #################################
 # Retrieve taxonomic identifiers for a given taxon name based on "itis"
 # Se puede usar las siguientes bases de datos db = c("itis", "ncbi", "eol", "col", "tropicos", "gbif", "nbn")
